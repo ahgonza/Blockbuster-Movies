bf39cc98928732a0cca7d203ca0c5c4eb2be5439
mando gonzalvez <ahgonza@pointpark.edu>
2023-03-13 18:30:48 -0400

    debug on logInModal, fix images appearing in navbar

--- js/loginbuttonlogic.js
+++ js/loginbuttonlogic.js
@@ -1,5 +1,6 @@
 function openLogin() {
     console.log(document.getElementById("overlay"));
     document.getElementById("overlay").style.visibility= 'visible';
-    document.getElementById("logInModal").style.visibility= 'visible';
+    document.getElementById("logInModal").style.visibility= "visible";
+    document.getElementById("logInContainer").style.visibility= 'visible';
 }
