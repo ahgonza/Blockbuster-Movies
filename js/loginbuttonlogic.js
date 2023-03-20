65b46b412662b4b10dd2d5725ad79bd6a6dd0cdc
Mando Gonzalvez <mandogonzalvez@mandos-air.pointpark.edu>
2023-03-20 12:00:20 -0400

    change logInModal opacity, add cart.html, give logIn form submit function

--- js/loginbuttonlogic.js
+++ js/loginbuttonlogic.js
@@ -4,3 +4,17 @@ function openLogin() {
     document.getElementById("logInModal").style.visibility= "visible";
     document.getElementById("logInContainer").style.visibility= 'visible';
 }
+
+function validateLogIn() {
+    const usernameInput = document.getElementById("username");
+    let isUsernameValid = true;
+
+    if (usernameInput.value.length < 1) {
+        isUsernameValid = false;
+    } else {
+        isUsernameValid = true;
+    }
+    
+    return isUsernameValid
+}
+    
