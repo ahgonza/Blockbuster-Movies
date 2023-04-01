8969e5b4af1ef3aa1179d7d6be12af8194d4c6a1
Vince Smykal <you@example.com>
2023-04-01 04:06:12 -0400

    Created gallery code

--- js/imageGallery.js
+++ js/imageGallery.js
@@ -1,3 +1,4 @@
+/*
 var dotmGallery = [];
 var tlkGallery=[];
 
@@ -10,4 +11,25 @@ function backImg(gallery){
     for(let i = gallery.length; i>0;i--){
         document.getElementById(img).src=gallery[i];
     }
+}
+old code*/ 
+var m1Gallery = ["images/movie1Gallery/tGallery1.png", "images/movie1Gallery/tGallery2.jpg","images/movie1Gallery/tGallery3.jpg"]
+var dotmGallery = [];
+var tlkGallery=[];
+var counter=0;
+function newImage(gallery){
+    var img = document.getElementById("img");
+    counter++;
+    if(counter>2){
+        counter= 0;
+    }
+    img.src=gallery[counter];
+}
+function backImage(gallery){
+    var img = document.getElementById("img");
+    counter--;
+    if(counter<0){
+        counter= 2;
+    }
+    img.src=gallery[counter];
 }
