dc042f5396a432f01d0e8dcb1ab40be5b8aad79b
Mando Gonzalvez <mandogonzalvez@mandos-air.pointpark.edu>
2023-04-03 19:00:34 -0400

    implement cart logic, cart onClick functional, display cart functional

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,34 @@
 function retrieveCart() {
+    if (localStorage.length === 0) {
+        alert("Cart is empty!");
+        return false;
+    } else {
+    window.location.href='cart.html';
+    }
+};
+
+function renderCart() {
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         const value = localStorage.getItem(key);
         var cartMovie = JSON.parse(value);
-        console.log(cartMovie);
-
         const writeCart =`
-        <strong>Name:</strong> ${cartMovie.name}
+        <div class="cart_image">${cartMovie.image}</div>
+        <div class="cart_info"><p>
+        <strong>${cartMovie.name}</strong></p>
+        <p>
+        <strong>Quantity:</strong> ${cartMovie.quantity}
+        </p>
+        <p>
+        <strong>Price:</strong> ${cartMovie.price}
+        </div>
+
         `;
-        document.getElementById("cart").innerHTML += writeCart;
-    }
-};
+
+        document.getElementById("cart").innerHTML = writeCart; 
+        }
+    };
+    
+    if (window.location.href.indexOf("cart.html") > -1) {
+        renderCart();
+    };
--- js/imageGallery.js
+++ js/imageGallery.js
@@ -6,20 +6,23 @@ var galleryMovies = {
         year: 2007, 
         rating: "7 out of 10",
         price: 1000000,
+        image: "<img src='images/transformers200.jpg' height='200' width='150'>"
     }, 
     'Transformers: Dark of the Moon': {
         name: "Transformers: Dark of the Moon",
         synopsis: "Sam Witwicky (Shia LaBeouf) and his new girlfriend, Carly (Rosie Huntington-Whiteley), join the fray when the evil Decepticons renew their longstanding war against the Autobots. Optimus Prime (Peter Cullen) believes that resurrecting ancient Transformer Sentinel Prime (Leonard Nimoy), once the leader of the Autobots, may lead to victory. ",
         year: 2011,
         rating: "6.2 out of 10",
-        price: 2000000
+        price: 2000000,
+        image: "<img src='images/transformers200.jpg' height='200' width='150'>"
     }, 
     'Transformers: The Last Knight': {
         name: "Transformers: The Last Knight",
         synopsis: "Humans are at war with the Transformers, and Optimus Prime is gone. The key to saving the future lies buried in the secrets of the past and the hidden history of Transformers on Earth. Now, it's up to the unlikely alliance of inventor Cade Yeager, Bumblebee, an English lord and an Oxford professor to save the world.",
         year: 2017,
         rating: "5.2 out of 10",
-        price: 3000000
+        price: 3000000,
+        image: "<img src='images/transformers200.jpg' height='200' width='150'>"
 }};
 var transformersGallery=["images/movie1Gallery/tGallery1.png", "images/movie1Gallery/tGallery2.png", "images/movie1Gallery/tGallery3.png"];
 var m1Gallery = ["images/movie1Gallery/tGallery1.png", "images/movie1Gallery/tGallery2.jpg","images/movie1Gallery/tGallery3.jpg"]
