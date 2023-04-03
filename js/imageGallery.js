173e605ac24e328072192b276276c7e7008510e2
Mando Gonzalvez <mandogonzalvez@Mandos-MacBook-Air.local>
2023-04-03 17:22:39 -0400

    add CSS styles to all pages, maintain login logic across all pages

--- js/imageGallery.js
+++ js/imageGallery.js
@@ -1,4 +1,26 @@
 //gallery code
+var galleryMovies = {
+    'Transformers (2007)': {
+        name: "Transformers",
+        synopsis: "The fate of humanity is at stake when two races of robots, the good Autobots and the villainous Decepticons, bring their war to Earth. The robots have the ability to change into different mechanical objects as they seek the key to ultimate power. Only a human youth, Sam Witwicky (Shia LaBeouf) can save the world from total destruction.",
+        year: 2007, 
+        rating: "7 out of 10",
+        price: 1000000,
+    }, 
+    'Transformers: Dark of the Moon': {
+        name: "Transformers: Dark of the Moon",
+        synopsis: "Sam Witwicky (Shia LaBeouf) and his new girlfriend, Carly (Rosie Huntington-Whiteley), join the fray when the evil Decepticons renew their longstanding war against the Autobots. Optimus Prime (Peter Cullen) believes that resurrecting ancient Transformer Sentinel Prime (Leonard Nimoy), once the leader of the Autobots, may lead to victory. ",
+        year: 2011,
+        rating: "6.2 out of 10",
+        price: 2000000
+    }, 
+    'Transformers: The Last Knight': {
+        name: "Transformers: The Last Knight",
+        synopsis: "Humans are at war with the Transformers, and Optimus Prime is gone. The key to saving the future lies buried in the secrets of the past and the hidden history of Transformers on Earth. Now, it's up to the unlikely alliance of inventor Cade Yeager, Bumblebee, an English lord and an Oxford professor to save the world.",
+        year: 2017,
+        rating: "5.2 out of 10",
+        price: 3000000
+}};
 var transformersGallery=["images/movie1Gallery/tGallery1.png", "images/movie1Gallery/tGallery2.png", "images/movie1Gallery/tGallery3.png"];
 var m1Gallery = ["images/movie1Gallery/tGallery1.png", "images/movie1Gallery/tGallery2.jpg","images/movie1Gallery/tGallery3.jpg"]
 var dotmGallery = [];
@@ -23,8 +45,7 @@ function backImage(gallery){
 //summary code
 function m1Sum(){
     var sumPopup = window.open("","","width=400,height=200,left=100,top=50");
-    sumPopup.document.write("The fate of humanity is at stake when two races of robots, the good Autobots and the villainous Decepticons, bring their war to Earth. The robots have the ability to change into different mechanical objects as they seek the key to ultimate power. Only a human youth, Sam Witwicky (Shia LaBeouf) can save the world from total destruction.");
-    return sumPopup;
+    sumPopup.document.write(galleryMovies["Transformers (2007)"].synopsis);
 }
 
 document.getElementById("addToCart").addEventListener('click', function() {
@@ -41,4 +62,6 @@ document.getElementById("addToCart").addEventListener('click', function() {
         quantity: currentCartMovie.quantity + 1 }
         console.log(cartMovie);
         localStorage.setItem(movieTitle,JSON.stringify(cartMovie));
-}});
+}
+    alert("added to cart!");
+});
